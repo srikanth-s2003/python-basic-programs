ae5e969219be4e3bbf883b9305a5df1adaa98744
Srujan <71495527+SrujanJagadeesh@users.noreply.github.com>
2023-10-22 04:51:48 -0400

    simple interest

---
+++ simple interest.py
@@ -0,0 +1,9 @@
+rint("the rate of interest \n")
+
+p=int(input("Enter the principal amount (p): "))
+t=int(input("Enter the tenure of interest (t): "))
+r=int(input("Enter the rate of interest (r) : "))
+
+print("the rate of interest is ",(p*t*r)//100)
+
+
